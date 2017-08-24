6b07d704575d29f3abf762f8127eaaa5e58de718
Lucas Nogueira <lukscasanova@gmail.com>
2017-08-24 22:45:14 +0000

    more runs

--- main.py
+++ main.py
@@ -127,8 +127,8 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
             _, loss = sess.run([train_op, cross_entropy_loss],
                 feed_dict = {input_image: batch_images,
                             correct_label: batch_labels,
-                            keep_prob: 0.5, 
-                            learning_rate: 1e-5})
+                            keep_prob: 0.75, 
+                            learning_rate: 1e-4})
             print("Batch", i)
             print("Loss {:.5f}...".format(loss))
             i+=1
@@ -176,8 +176,8 @@ def run():
         # TODO: Train NN using the train_nn function
         sess.run(tf.global_variables_initializer())
 
-        epochs = 2
-        batch_size = 4
+        epochs = 15
+        batch_size = 16
         # learning_rate = 0.0001
 
         # keep_prob = keep_prob.eval()
